e23961a1587f7960b8b64d612e988825d7af4d9e
Profnird <52837348+Profnird@users.noreply.github.com>
2021-09-10 16:23:14 +0100

    update main

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,3 +11,12 @@ let nav = () =>{
         logo.className = "logo";
     }
 }
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/613b73c7d326717cb680cb66/1ff83ohrm';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
